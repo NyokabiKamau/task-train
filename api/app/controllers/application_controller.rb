5c1b73e4cd462f2e068735ecb80abac1dcc188b0
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-23 11:17:08 +0300

    done with login endpoint

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
     
     def app_response(message: 'success', status: 200, data: nil)
         render json: {
@@ -7,4 +8,25 @@ class ApplicationController < ActionController::API
         }, status: status
     end
 
+    # store the id in the session
+    def save_user(id)
+        session[:uid] = id
+        session[:expiry] = 6.hours.from_now
+    end
+
+    # delete a user id in session
+    def remove_user
+        session.delete(:uid)
+        session[:expiry] = Time.now
+    end
+    
+    # check for session expiry
+    def session_expired?
+        session[:expiry] ||= Time.now
+        time_diff = (Time.parse(session[:expiry]) - Time.now).to_i
+        unless time_diff > 0
+            app_response(message: "failed", status: 401, data: {
+                info: 'Your session has expired. Please login again to continue'})
+        end
+    end
 end
--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
+    before_action :session_expired?, only: [:check_login_status]
 
     def register
         user = User.create(user_params)
         if user.valid?
+            save_user(id: user.id)
             app_response(message: 'Registration was successful', data: user, status: :created)
         else
             app_response(message: 'Something went wrong during registration', data: user.errors, status: :unprocessable_entity)
         end
+    end
+
+    def login
+        sql = "username = :username OR email = :email"
+        user = User.where(sql, { username: user_params[:username], email: user_params[:email]}).first
+        if user&.authenticate(user_params[:password])
+            save_user(id: user.id)
+            app_response(message: 'Registration was successful', data: user, status: :ok)
+        else
+            app_response(message: 'Invalid username/email or password', status: :unauthorized)
+        end
+    end 
+
+    def logout
+        remove_user
+        app_response(message: 'Logout successful')
+    end
+
+    def check_login_status
+        app_response(message: 'Success', status: :ok)
     end
 
     private
@@ -14,4 +36,5 @@ class UsersController < ApplicationController
     def user_params
         params.permit(:username, :email, :password)
     end
+
 end
